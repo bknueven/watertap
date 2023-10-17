085b215dc6c54ad2de191bdfe5a1c9781f5648b6
Bernard Knueven <Bernard.Knueven@nrel.gov>
2023-10-17 10:24:10 -0600

    consistent expressions / vars across both costing packages

--- watertap/examples/flowsheets/full_treatment_train/flowsheet_components/chemistry/posttreatment_ideal_naocl_chlorination_block.py
+++ watertap/examples/flowsheets/full_treatment_train/flowsheet_components/chemistry/posttreatment_ideal_naocl_chlorination_block.py
@@ -477,7 +477,7 @@ ideal_naocl_reaction_config = {
 # End reaction_config definition
 
 # Get default solver for testing
-solver = get_solver(options={"tol": 1e-11})
+solver = get_solver(options={"tol": 1e-11, "ma27_pivtol": 0.5})
 
 
 def build_ideal_naocl_prop(model):
