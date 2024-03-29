09036cdee66b6135da69f5b1c1ad2fe261c806be
Bernard Knueven <Bernard.Knueven@nrel.gov>
2024-03-29 11:27:32 -0600

    Revert "try not restoring values when FBBT fails"
    This reverts commit 498f3e0785f0562724a72b3a4cf9ed0510a1e793.

--- watertap/core/plugins/solvers.py
+++ watertap/core/plugins/solvers.py
@@ -249,7 +249,7 @@ class IpoptWaterTAPFBBT:
             )
         except:
             # cleanup before raising
-            # self._restore_values()
+            self._restore_values()
             self._restore_bounds()
             raise
 
